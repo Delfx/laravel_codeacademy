c63c20e461bd7ce8f4fb5de0e8eddfc1a117042e
Delfx <motiejunas.lukas@gmail.com>
2022-10-27 21:19:12 +0300

    added bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,10 @@
 import './bootstrap';
 
+import '../sass/app.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
