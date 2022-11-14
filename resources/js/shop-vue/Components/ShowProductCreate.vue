3ed7fd01d56391c0cdbb81d095122d3a74a47b38
Delfx <motiejunas.lukas@gmail.com>
2022-11-15 00:44:48 +0200

    added validation

--- resources/js/shop-vue/Components/ShowProductCreate.vue
+++ resources/js/shop-vue/Components/ShowProductCreate.vue
@@ -1,21 +1,45 @@
 <script setup>
 import axios from 'axios';
-import { ref } from 'vue';
+import { ref, reactive, computed } from 'vue';
 import router from '../router';
 
 
 const selectedCategories = ref('0')
 const product = ref([])
 const productCategories = ref([])
+const validationError = reactive({
+    name: '',
+    description: '',
+    price: '',
+    identifier: '',
+    category_id: ''
+})
 
 axios.get('/api/v1/productcategory').then(response => {
     productCategories.value = response.data.data;
 });
 
+const errorClassName = computed(() => {
+    return validationError.name == '' ? false : true
+})
+
+const errorClassDescription = computed(() => {
+    return validationError.description == '' ? false : true
+})
+
+const errorClassPrice = computed(() => {
+    return validationError.price == '' ? false : true
+})
+
+const errorClassIdentifier = computed(() => {
+    return validationError.identifier == '' ? false : true
+})
+
+const errorClassCategory = computed(() => {
+    return validationError.category_id == '' ? false : true
+})
+
 function showProduct() {
-
-    // console.log(selectedCategories.value);
-
     axios.post('/api/v1/product/create/', {
         name: product.value.name,
         description: product.value.description,
@@ -24,10 +48,15 @@ function showProduct() {
         category_id: selectedCategories.value,
     })
         .then(function (response) {
-            router.push({name: 'products.index'})
+            router.push({ name: 'products.index' })
             console.log(response);
         })
         .catch(function (error) {
+            validationError.name = error.response.data.errors.name[0]
+            validationError.description = error.response.data.errors.description[0]
+            validationError.price = error.response.data.errors.price[0]
+            validationError.identifier = error.response.data.errors.identifier[0]
+            // validationError.category_id = error.response.data.errors.category_id[0]
             console.log(error.response.data.errors);
         });
 }
@@ -41,12 +70,20 @@ function showProduct() {
             <label for="formGroupInput" class="form-label">Product name</label>
             <input v-model="product.name" type="text" class="form-control">
         </div>
+
+        <div :class="{ alert: errorClassName, 'alert-danger': errorClassName}" role="alert">
+            {{ validationError.name }}
+        </div>
+
         <div class="mb-3">
             <label for="FormControlTextarea" class="form-label">Product Description</label>
             <textarea v-model="product.description" class="form-control" id="FormControlTextareaDescription"
                 rows="3"></textarea>
         </div>
 
+        <div :class="{ alert: errorClassDescription, 'alert-danger': errorClassDescription}" role="alert">
+            {{ validationError.description }}
+        </div>
 
         <div class="mb-3">
             <label for="formGroupInput" class="form-label">Product Category</label>
@@ -59,10 +96,18 @@ function showProduct() {
             </select>
         </div>
 
+        <div :class="{ alert: errorClassCategory, 'alert-danger': errorClassCategory}" role="alert">
+            {{ validationError.category_id }}
+        </div>
+
         <div class="mb-3">
             <label for="formGroupInput" class="form-label">Product identifier</label>
             <input v-model="product.identifier" type="text" class="form-control" id="formGroupInputPrice"
                 placeholder="Product identifier">
+        </div>
+
+        <div :class="{ alert: errorClassIdentifier, 'alert-danger': errorClassIdentifier}" role="alert">
+            {{ validationError.identifier }}
         </div>
 
         <div class="mb-3">
@@ -71,9 +116,13 @@ function showProduct() {
                 placeholder="Product price">
         </div>
 
+        <div :class="{ alert: errorClassPrice, 'alert-danger': errorClassPrice}" role="alert">
+            {{ validationError.price }}
+        </div>
+
         <button @click="showProduct" class="btn btn-info">Save</button>
 
-        <router-link :to="{ name: 'products.index'}" class="btn btn-danger m-2"> Back </router-link>
+        <router-link :to="{ name: 'products.index' }" class="btn btn-danger m-2"> Back </router-link>
     </div>
 
 </template>
